b4873f606c5061502ff3262fcc0ef2177bfd8dd7
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2023-06-28 10:22:04 +1200

    useState &  useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [resourceType, setResourceType] = useState('posts');
+  console.log('render');// console.log('reder') will be printed whenever the function App is rendered.
+
+  useEffect(()=>{
+    console.log('resourcetype changed')
+  }, [resourceType]) // useEffect works whenever [resourceType] is changed
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <button onClick={() => setResourceType("posts")}> Posts</button>
+        <button onClick={() => setResourceType("users")}> Users</button>
+        <button onClick={() => setResourceType("comments")}> Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+    </>
   );
+  
 }
 
 export default App;
