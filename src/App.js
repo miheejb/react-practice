24297190b19d2b648ae73f43b4a67d49870fd97e
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2023-06-28 11:49:45 +1200

    clean up useEffect

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
  
   useEffect(()=> {
     window.addEventListener('resize', handleResize)
+    
+    // clean  up the code ... avoid constantly using the useEffect unnecessily???
     return ()=>{
       window.removeEventListener('resize', handleResize)
     }
